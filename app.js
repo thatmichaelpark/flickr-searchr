b8a49467932c1e60f8a4a07f8168daee8b9f0c0d
Michael Park <thatmichaelpark@gmail.com>
2016-04-03 08:19:02 -0700

    able to show pictures

--- app.js
+++ app.js
@@ -11,17 +11,26 @@ angular.module('FlickrSearchrApp', [])
 			return;
 		}
 
+		$scope.pix = [];
+
 		var url = 'https://api.flickr.com/services/rest/';
 		var params = {
 			api_key: 'df4aa9f4273411fa40e04118919f080f',
 			method: 'flickr.photos.search',
 			format: 'json',
 			jsoncallback: 'JSON_CALLBACK',
-			tags: 'kittens'
+			tags: 'kittens, trees'
 		}
 		$http.jsonp(url, {params: params})
 		.success(function (response) {
-			console.log(response);;;
+			if (response.stat === 'ok') {
+				console.log(response.photos.photo.length);;;
+				response.photos.photo.forEach(function (photo) {
+					$scope.pix.push(makePhotoUrl(photo));
+				});
+			} else {
+				// error: response.message
+			}
 		})
 		.error(function (x, y) {
 			console.log(x, y);;;
@@ -31,5 +40,10 @@ angular.module('FlickrSearchrApp', [])
 		$scope.searchForm.$setPristine();
 	}
 
+	function makePhotoUrl(p) {
+	//	https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+		return 'https://farm' + p.farm + '.staticflickr.com/' + p.server +
+				'/' + p.id + '_' + p.secret + '_t.jpg';
+	}
 });
 
