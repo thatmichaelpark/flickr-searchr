5962d2cc7d470c2cb7a1ebfe22ee559d73828efa
Michael Park <thatmichaelpark@gmail.com>
2016-04-02 22:18:30 -0700

    starting form

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+angular.module('FlickrSearchrApp', [])
+.controller('FlickrSearchrCtrl', function ($scope, $timeout, $http) {
+$scope.submit = function () {
+	alert('hi');;;
+}
+});
+
